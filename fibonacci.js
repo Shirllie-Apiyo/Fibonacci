de0c582ac887d24376e96b5774d873cafaf0a858
Shirllie-Apiyo <apraise320@gmail.com>
2022-02-14 23:37:33 +0300

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+let num1 = function(n) {
+    if (n === 1) {
+      return [0, 1];
+    } else {
+      let arr = num1(n - 1);
+      arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+      return arr;
+    }
+  };
+  
+  console.log(fib(8));
